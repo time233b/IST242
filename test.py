0940a7f948b15da8057609ee89adbc9ee5d17ce0
time233b <fsjaylo@gmail.com>
2026-02-05 14:25:46 -0500

    test file

---
+++ test.py
@@ -0,0 +1,38 @@
+"""
+Testing library
+""" 
+import unittest
+from unittest.mock import patch
+from io import StringIO
+#import our user made function for testing
+from personal_library import(
+    add_book,
+    remove_book,
+    list_book,
+    search_book
+)
+#test class 
+class TestPersonallibrary(unittest.TestCase):
+    #test case for add book function
+    def test_add_book_normal_case(self):
+        library =[]
+        with patch("builtins.input",return_value ="Dune"):
+           add_book(library)
+        
+        self.assertEqual(library, ["Dune"])
+
+    def test_list_more_than_one_book(self):
+        library= ["Dune","Dune Messiah"]
+        with patch("sys.stdout",new=StringIO()) as out:
+            list_book(library)
+            output = out.getvalue()
+        self.assertIn("Dune", output)
+        self.assertIn("Dune Messiah", output)
+      
+
+    
+
+#call the test class main method
+
+if __name__ == "__main__":
+    unittest.main()
