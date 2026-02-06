e3819d6d1abb217f853ebe4feec033f8b6e340f5
jpd6423 <jpd6423@psu.edu>
2026-02-06 08:19:28 -0500

    Added a test case for duplicates in addBook
    Needs changing since it failed

--- test.py
+++ test.py
@@ -21,6 +21,16 @@ class TestPersonallibrary(unittest.TestCase):
         
         self.assertEqual(library, ["Dune"])
 
+    #test case for duplicates in add book function (needs fixing)
+    def test_add_book_duplicate_case(self):
+        library =[]
+        with patch("builtins.input",return_value ="Dune"):
+           add_book(library)
+           add_book(library)
+        
+        self.assertEqual(library, ["Dune"])
+
+    #test case of list book function
     def test_list_more_than_one_book(self):
         library= ["Dune","Dune Messiah"]
         with patch("sys.stdout",new=StringIO()) as out:
